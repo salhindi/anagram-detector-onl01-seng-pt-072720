6b32a5379bbb13acdac54b73649387fac7c16659
Sara A <calisaraalhindi@gmail.com>
2020-08-25 20:52:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :diaper
+  
+  def initialize(diaper)
+    @diaper = diaper
+  end
+  
+  def match(anagrams)
+    anagrams.select {|x| x.split("").sort == diaper.split("").sort}
+  end
+    
+end
